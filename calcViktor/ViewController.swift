fadbf1b8cfc07ce42bdce29deed50a4a6e15c643
Mac27 <mac27@iMac-de-Mac27.local>
2016-02-10 17:33:53 -0600

    First commit

---
+++ calcViktor/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  calcViktor
+//
+//  Created by Mac27 on 10/02/16.
+//  Copyright © 2016 viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    //Muestra el resultado asociado al label
+    @IBOutlet weak var Screen: UILabel!
+    
+    
+    
+    //obtiene el numero oprimido
+    @IBAction func numeros(sender: AnyObject) {
+        
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func puntoDecimal(sender: AnyObject) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func operaciones(sender: AnyObject) {
+        
+        
+    }
+    
+    
+    @IBAction func limpiar(sender: AnyObject) {
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
